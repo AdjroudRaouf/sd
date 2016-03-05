d7ba05766ca5413246ad555ce4438061ee903df3
Raouf <adjroud.abderaouf@gmail.com>
2016-03-05 16:22:38 +0100

    Update sd.java

--- sd/src/sd/sd.java
+++ sd/src/sd/sd.java
@@ -3,7 +3,6 @@ package sd;
 import java.util.Scanner;
 
 public class sd {
-	//rafaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
